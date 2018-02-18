230275e0d4049b8d2cebaa6ed762894c31a12ccf
daich <daichithirteen@gmail.com>
2018-02-18 14:24:28 +0900

    ToolBar fixed!

--- app/src/main/java/com/hmdroid/kaizucalc/MainActivity.java
+++ app/src/main/java/com/hmdroid/kaizucalc/MainActivity.java
@@ -3,6 +3,7 @@ package com.hmdroid.kaizucalc;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        toolbar.setTitle("日本円から貝塚円");
+        setSupportActionBar(toolbar);
     }
 
     public void kaizuka(View view) {
--- app/src/main/java/com/hmdroid/kaizucalc/about.java
+++ app/src/main/java/com/hmdroid/kaizucalc/about.java
@@ -2,6 +2,8 @@ package com.hmdroid.kaizucalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class about extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class about extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar toolbar=findViewById(R.id.toolbar6);
+        toolbar.setTitle("このアプリについて　");
+        setSupportActionBar(toolbar);
     }
 }
