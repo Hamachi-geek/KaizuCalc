02af5e9c4301ec7e6cddeb67cbb5c9d96dfbfe68
daich <daichithirteen@gmail.com>
2018-04-30 11:13:28 +0900

    changed text

--- app/src/main/java/com/hmdroid/kaizucalc/Main2Activity.java
+++ app/src/main/java/com/hmdroid/kaizucalc/Main2Activity.java
@@ -13,7 +13,8 @@ import static java.math.BigDecimal.ROUND_HALF_UP;
 
 
 public class Main2Activity extends AppCompatActivity {
-EditText kaizuka;
+    EditText kaizuka;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +23,13 @@ EditText kaizuka;
 
     public void japan(View view) {
         kaizuka = findViewById(R.id.kaizuka);
-        if(kaizuka.getText().toString().equals("")){
-            Toast.makeText(this,"何か入力するだで。",Toast.LENGTH_SHORT);
-        }else {
+        if (kaizuka.getText().toString().equals("")) {
+            Toast.makeText(this, "何か入力するだで", Toast.LENGTH_SHORT);
+        } else {
             BigDecimal k = new
                     BigDecimal(kaizuka.getText().toString());
             BigDecimal big3 = new BigDecimal("35");
             double j = k.divide(big3, 3, ROUND_HALF_UP).doubleValue();
-
 
             AlertDialog.Builder alert01 = new AlertDialog.Builder(this);
             alert01.setTitle("警告:");
